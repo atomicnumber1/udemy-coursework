9ebf3f4f6fb307ec915511fba3721bfd1a4a4965
atomicnumber1 <0xproton@gmail.com>
2017-12-23 20:18:06 +0530

    Completed Section 13

---
+++ iOS/app2/Checklists/Checklists/ChecklistViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Akash on 22/12/17.
+//  Copyright © 2017 atomicnumber1. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = cell.accessoryType == .none ? .checkmark : .none
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        let label = cell.viewWithTag(1000) as! UILabel
+        let remainder = indexPath.row % 5
+        
+        switch(remainder) {
+        case 0:
+            label.text = "Walk the dog"
+        case 1:
+            label.text = "Brush my teeth"
+        case 2:
+            label.text = "Learn iOS development"
+        case 3:
+            label.text = "Soccer practice"
+        default:
+            label.text = "Eat ice cream"
+        }
+        
+        return cell
+    }
+
+}
+
